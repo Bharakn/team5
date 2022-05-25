29b353afa66354f2803304a00b3837708bd984df
Venkysvry <87991111+Venkysvry@users.noreply.github.com>
2022-05-25 23:45:43 +0530

    finalupdate

--- financial_org_rating_system/lib/src/main/java/com/cg/financial_org_rating_system/FinancialOrgRatingSystemApplication.java
+++ financial_org_rating_system/lib/src/main/java/com/cg/financial_org_rating_system/FinancialOrgRatingSystemApplication.java
@@ -3,12 +3,7 @@ package com.cg.financial_org_rating_system;
 public class FinancialOrgRatingSystemApplication {
 
 	public static void main(String[] args) {
-       System.out.println("This is bharath");
-       System.out.println("thi is 2nd update");
-       System.out.println("this is 3rd update");
-       System.out.println("this is 4th update");
-       System.out.println("this is 5th update");
-       System.out.println("this is 6th update");
+       
 	}
 
 }
