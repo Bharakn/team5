c37ada77162129f30d59a828530c813e7eb4d844
Bharakn <103725471+Bharakn@users.noreply.github.com>
2022-05-25 22:55:20 +0530

    update1

--- financial_org_rating_system/lib/src/main/java/com/cg/financial_org_rating_system/FinancialOrgRatingSystemApplication.java
+++ financial_org_rating_system/lib/src/main/java/com/cg/financial_org_rating_system/FinancialOrgRatingSystemApplication.java
@@ -3,7 +3,7 @@ package com.cg.financial_org_rating_system;
 public class FinancialOrgRatingSystemApplication {
 
 	public static void main(String[] args) {
-
+       System.out.println("This is bharath");
 
 	}
 
