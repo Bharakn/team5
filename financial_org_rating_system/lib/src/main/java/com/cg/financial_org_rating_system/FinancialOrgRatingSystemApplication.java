9833c5b9b96afcb8aaeab7f0a6e80158635674a1
Bharakn <103725471+Bharakn@users.noreply.github.com>
2022-05-26 17:54:28 +0530

    alldeleted

--- financial_org_rating_system/lib/src/main/java/com/cg/financial_org_rating_system/FinancialOrgRatingSystemApplication.java
+++
@@ -1,9 +0,0 @@
-package com.cg.financial_org_rating_system;
-
-public class FinancialOrgRatingSystemApplication {
-
-	public static void main(String[] args) {
-       
-	}
-
-}
---
+++ financial_org_rating_system/lib/src/main/java/sample/Samplecheck.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class Samplecheck {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+
+	}
+
+}
